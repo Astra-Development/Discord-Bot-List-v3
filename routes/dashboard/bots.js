cb82a37fe2d43e87c9009df286114b9735003a46
LuckyTarget <luckytarget000@gmail.com>
2023-04-03 19:03:44 +0300

    Modified some user.id checks for admin page access

--- routes/dashboard/bots.js
+++ routes/dashboard/bots.js
@@ -11,7 +11,7 @@ app.get("/dashboard/bots", async (req, res) => {
         message: "You need to be logged in to view this page."
     });
 
-    if (req.user.id != "857177733398265876") return res.render("404", {
+    if (!global.config.client.owners.includes(req.user.id)) return res.render("404", {
         bot: global.client ? global.client : null,
         path: req.path,
         user: req.isAuthenticated() ? req.user : null,
